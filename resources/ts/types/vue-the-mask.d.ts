efde27e2794ddf257c5d6ce6cf785cbd82c3ab09
PlomoChavez <jesus.chavez@loyola.edu.mx>
2025-05-15 23:34:13 -0600

    Limpieza

--- resources/ts/types/vue-the-mask.d.ts
+++
@@ -1,6 +0,0 @@
-// filepath: /Users/plomochavez/Documents/Palmakt/delfaro/resources/ts/types/vue-the-mask.d.ts
-declare module "vue-the-mask" {
-  import { Plugin } from "vue";
-  const VueTheMask: Plugin;
-  export default VueTheMask;
-}
