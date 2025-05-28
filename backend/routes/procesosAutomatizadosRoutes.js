f6f4335ae6d61483f2397ecf4f72d2f46edbb181
PlomoChavez <jesus.chavez@loyola.edu.mx>
2025-05-27 19:51:45 -0600

    Avances

--- backend/routes/procesosAutomatizadosRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const procesosAutomatizadosController = require('../controllers/procesosAutomatizadosController');
-
-router.post('/api/procesos/bot', procesosAutomatizadosController.obtenerTituloGoogle);
-
-module.exports = router;
